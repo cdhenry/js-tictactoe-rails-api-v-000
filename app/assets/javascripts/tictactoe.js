cf823b9ee6d410580b77592521e65e7f97bf07e5
Chris Henry <chrishenry@protonmail.com>
2018-05-05 17:49:37 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -3,7 +3,7 @@ const WIN_COMBINATIONS = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8],
 
 let currentGame = 0;
 let currentState = ['','','','','','','','',''];
-let turn = 0;
+var turn = 0;
 
 $(function() {
   attachListeners();
